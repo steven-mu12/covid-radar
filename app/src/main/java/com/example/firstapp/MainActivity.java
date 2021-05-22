24c1b76851a705ad2dceffef3081e36c410ccf93
Emre Cenk <emrecenk9@gmail.com>
2021-05-22 13:02:29 -0400

    added dynamic scrollviews

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -13,21 +13,47 @@ import android.os.Bundle;
 
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.content.Intent;
 
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setContentView(R.layout.activity_main);
+
+        LinearLayout ll = (LinearLayout) findViewById(R.id.log_scroll);
+        TextView tv1 = new TextView(this);
+        tv1.setText("This is tv1");
+
+        ll.addView(tv1);
     }
 
     @SuppressLint("SetTextI18n")
     public void change_box(View given_view){
+        setContentView(R.layout.activity_main);
 
+        LinearLayout ll = (LinearLayout) findViewById(R.id.log_scroll);
+        TextView tv1 = new TextView(this);
+        tv1.setText("This is tv1");
+
+        ll.addView(tv1);
+
+
+
+
+
+
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println(i);
+//        }
 //        EditText name_given = findViewById(R.id.person_name);
 //        String name_text = name_given.getText().toString();
 //
