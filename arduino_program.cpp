58eb4a84b27932e7da3222e7bccccf98e6d60b5c
Steven Mu <xsmu0922@gmail.com>
2021-05-23 12:13:14 -0400

    Update arduino_program.cpp

--- arduino_program.cpp
+++ arduino_program.cpp
@@ -1,5 +1,6 @@
 #include <Servo.h>
 #include <NewPing.h>
+#include <SoftwareSerial.h>
 
 Servo servo;
 const int trigPin = 12;
@@ -8,37 +9,88 @@ const int echoPin = 13;
 int distance;
 int setRange;
 int duration;
+int detectedDistance;
+
+const int RX_PIN = 0;
+const int TX_PIN = 1;
+SoftwareSerial serial(RX_PIN, TX_PIN);
+char commandChar;
+
+
+// SETUP
 
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  
   servo.attach(9); 
   Serial.begin(9600);
+
+  randomSeed(analogRead(0));
 }
 
+
+// LOOP
+
+
 void loop() {
-  // servo code for the spin mechanism 
 
-  for (int i=0; i<18; i++){
-    servo.write(45);
-    delay(150);
-    servo.write(90);
-    delay(500);
+ for (int i=0; i<18; i++){
+   servo.write(45);
+   delay(150);
+   servo.write(90);
+   delay(500);
 
-    distance = calculateDistance();
-    Serial.print(distance);
-  }
-  
-  for (int u=0; u<18; u++){
-    servo.write(135);
-    delay(150);
-    servo.write(90);
-    delay(500);
+   distance = calculateDistance();
 
-    distance = calculateDistance();
-    Serial.print(distance);
-  }
+   if (distance < 200){
+     detectedDistance = distance;
+
+     if(Serial.available()){
+       commandChar = Serial.read();
+       switch(commandChar){
+         case '*':
+         Serial.print(detectedDistance);
+         break;
+       }  
+     }
+   }  
+ }
+
+
+ for (int i=0; i<18; i++){
+   servo.write(135);
+   delay(150);
+   servo.write(90);
+   delay(500);
+
+   distance = calculateDistance();
+
+   if (distance < 200){
+     detectedDistance = distance;
+
+     if(Serial.available()){
+       commandChar = Serial.read();
+       switch(commandChar){
+         case '*':
+         Serial.print(detectedDistance);
+         break;
+       }  
+     }
+   }
+ }
+
+
+    if(Serial.available()){
+      commandChar = Serial.read();
+      Serial.print(commandChar);
+      switch(commandChar){
+        case '*':
+        Serial.print(random(1000) + "#");
+        break;
+      }  
+    } 
 }
 
 int calculateDistance(){
@@ -50,6 +102,6 @@ int calculateDistance(){
 
   duration = pulseIn(echoPin, HIGH);
 
-  distance = duration*0.034/2;
+  distance = duration/58;
   return distance;
 }
